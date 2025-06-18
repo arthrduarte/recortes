3ce3a1c2c35db279d5080fe30ab7e9d7065d0b9c
Arthur Duarte <tuiduarte@gmail.com>
2025-06-18 00:08:21 -0400

    chore: add new style rules

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,19 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = '#7BA0E4';
+const tintColorDark = '#7BA0E4';
 
 export default {
   light: {
     text: '#000',
     background: '#fff',
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: '#7BA0E4',
     tabIconSelected: tintColorLight,
   },
   dark: {
     text: '#fff',
     background: '#000',
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: '#7BA0E4',
     tabIconSelected: tintColorDark,
   },
 };
